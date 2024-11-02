081aba2118749f49fa26f3d45b59111719d07add
ChengSeng-Tan <40026682+ChengSeng-Tan@users.noreply.github.com>
2024-11-02 10:14:09 +0800

    final

--- Home.py
+++ Home.py
@@ -36,8 +36,7 @@ def main():
                     <strong>IMPORTANT NOTICE</strong>: This web application is a prototype developed for educational purposes only.
                     The information provided here is <strong>NOT intended for real-world usage</strong> and should not be relied 
                     upon for making any decisions, especially those related to financial, legal, or healthcare matters.
-                    <strong>Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. 
-                    You assume full responsibility for how you use any generated output.</strong><br>
+                    <strong>Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output.</strong><br>
                     Always consult with qualified professionals for accurate and personalized advice.
                     """
         d = f"<p style='font-size:14px;'>{disclaimer}</p>"
